23c1912a5a5d722c1e2245faf6e788c22621cde6
pardeepsachdeva <pardeepsachdeva@yahoo.com>
2017-03-12 22:19:57 +0000

    Structure updated
    Updated the basic structure of the website

--- includes/menu.php
+++ includes/menu.php
@@ -1,18 +1,4 @@
-<!-- <div class="row">
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="training.php">Training</a></li>
-		<li><a href="development.php">Development</a></li>
-		<li><a href="placement.php">Placement</a></li>
-		<li><a href="products.php">Products</a></li>
-		<li><a href="contact.php">Contact</a></li>
-	</ul>
-</div>
- -->
-
-		
-        
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar" style="background-color:lightblue">
         <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
@@ -25,11 +11,11 @@
             <div class="collapse navbar-collapse" id="mainNavBar">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="index.php">Home</a></li>
-                    <li><a href="text/training.php">Training</a></li>
-                    <li><a href="text/development.php">Development</a></li>
-					<li><a href="text/placement.php">Placement</a></li>
-					<li><a href="text/products.php">Products</a></li>
-					<li><a href="text/contact.php">Contact</a></li>
+                    <li><a href="training.php">Training</a></li>
+                    <li><a href="development.php">Development</a></li>
+					<li><a href="placement.php">Placement</a></li>
+					<li><a href="products.php">Products</a></li>
+					<li><a href="contact.php">Contact</a></li>
                 </ul>
             </div>
 
